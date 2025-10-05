68087e33094ea2f7b841629cd0aa81965daa1ec6
Septieme7 <lordstiko7@gmail.com>
2025-10-05 19:45:20 +0200

    Mise a jour + Test IA GitHub

--- script.js
+++ script.js
@@ -53,3 +53,75 @@ function showAll() {
         <p>La valeur ben est donc ${ben} et py C tout...</p>
     `;
 }
+
+// Attendre que le DOM soit chargé
+document.addEventListener('DOMContentLoaded', function() {
+    // Récupérer tous les boutons
+    const buttons = document.querySelectorAll('.button-diagonal');
+    const showAllBtn = document.querySelector('.show-all');
+    const clearAllBtn = document.querySelector('.clear-all');
+
+    // Assigner les fonctions aux boutons diagonaux
+    if (buttons[0]) buttons[0].addEventListener('click', showH);
+    if (buttons[1]) buttons[1].addEventListener('click', showW);
+    if (buttons[2]) buttons[2].addEventListener('click', showV);
+    if (buttons[3]) buttons[3].addEventListener('click', showU);
+    if (buttons[4]) buttons[4].addEventListener('click', showBenPossible);
+    if (buttons[5]) buttons[5].addEventListener('click', showBenFinal);
+
+    // Assigner les fonctions aux boutons spéciaux
+    if (showAllBtn) showAllBtn.addEventListener('click', showAll);
+    if (clearAllBtn) clearAllBtn.addEventListener('click', clearResults);
+
+    // Mettre à jour le tableau des variables
+    updateVariablesTable();
+});
+
+// Fonction pour mettre à jour le tableau des variables
+function updateVariablesTable() {
+    const tbody = document.querySelector('.variables-table tbody');
+    if (tbody) {
+        tbody.innerHTML = `
+            <tr>
+                <td>x</td>
+                <td>${x}</td>
+                <td>Actif</td>
+            </tr>
+            <tr>
+                <td>y</td>
+                <td>${y}</td>
+                <td>Actif</td>
+            </tr>
+            <tr>
+                <td>z</td>
+                <td>${z}</td>
+                <td>Actif</td>
+            </tr>
+            <tr>
+                <td>h (x + y)</td>
+                <td>${h}</td>
+                <td>Calculé</td>
+            </tr>
+            <tr>
+                <td>w (x - y)</td>
+                <td>${w}</td>
+                <td>Calculé</td>
+            </tr>
+            <tr>
+                <td>v (x / y)</td>
+                <td>${v}</td>
+                <td>Calculé</td>
+            </tr>
+            <tr>
+                <td>u (x + y + h)</td>
+                <td>${u}</td>
+                <td>Calculé</td>
+            </tr>
+            <tr>
+                <td>ben</td>
+                <td>${ben}</td>
+                <td>Divine</td>
+            </tr>
+        `;
+    }
+}
